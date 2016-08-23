3694515d810d178ef700537971273d18be56a5df
laurence bonney <bonneyl@uk.ibm.com>
2016-08-23 16:23:38 +0100

    Allow applications to be run locally

--- backend/src/main/java/com/ibm/mqlight/sample/BackendWorker.java
+++ backend/src/main/java/com/ibm/mqlight/sample/BackendWorker.java
@@ -1,6 +1,6 @@
 // ******************************************************************
 //
-// Program name: mqlight_sample_frontend_web
+// Program name: mqlight_sample_backend
 //
 // Description:
 //
@@ -9,12 +9,12 @@
 // <copyright
 // notice="lm-source-program"
 // pids=""
-// years="2014"
+// years="2014, 2016"
 // crc="659007836" >
 // Licensed Materials - Property of IBM
 //
 //
-// (C) Copyright IBM Corp. 2014 All Rights Reserved.
+// (C) Copyright IBM Corp. 2014, 2016 All Rights Reserved.
 //
 // US Government Users Restricted Rights - Use, duplication or
 // disclosure restricted by GSA ADP Schedule Contract with
@@ -74,7 +74,12 @@ public class BackendWorker {
     try {
       logger.log(Level.INFO,"Creating an MQ Light client...");
 
-      mqlightClient = NonBlockingClient.create(null, new NonBlockingClientAdapter<Void>() {
+      String service = null;
+      if (System.getenv("VCAP_SERVICES") == null) {
+        service = "amqp://localhost:5672";
+      }
+      
+      mqlightClient = NonBlockingClient.create(service, new NonBlockingClientAdapter<Void>() {
 
         @Override
         public void onStarted(NonBlockingClient client, Void context) {
